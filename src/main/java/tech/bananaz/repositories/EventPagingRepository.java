5dab891d5dd1a2d4bb80c9b01105d7e921eb73c7
Aaron Renner <arennerwv@gmail.com>
2022-08-04 02:00:18 -0400

    1.1.0 Added reusable Discord and Twitter

--- src/main/java/tech/bananaz/repositories/EventPagingRepository.java
+++ src/main/java/tech/bananaz/repositories/EventPagingRepository.java
@@ -1,9 +1,25 @@
 package tech.bananaz.repositories;
 
+import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import tech.bananaz.enums.EventType;
 import tech.bananaz.models.Event;
 
 @Repository
-public interface EventPagingRepository extends PagingAndSortingRepository<Event, Long>, JpaSpecificationExecutor<Event> {}
+public interface EventPagingRepository extends PagingAndSortingRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+	
+	Event	    findById(long id);
+	boolean 	existsByIdAndConsumedFalse(long id);
+	List<Event> findByConfigIdAndConsumedFalseAndEventTypeOrderByCreatedDateAsc(long configId, EventType eventType);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE Event e SET e.consumed = 1, e.consumedBy = ?2 WHERE e.id = ?1 AND e.consumed = 0")
+	int		    updateByIdSetConsumedTrueAndConsumedBy(long id, String consumedBy);
+	
+}
