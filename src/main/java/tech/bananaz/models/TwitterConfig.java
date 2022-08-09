28f678bfa8c80c02a31ff7de9088b07779c46236
Aaron Renner <arennerwv@gmail.com>
2022-08-08 23:58:11 -0400

    1.3.1 moved decryption bot side

--- src/main/java/tech/bananaz/models/TwitterConfig.java
+++ src/main/java/tech/bananaz/models/TwitterConfig.java
@@ -1,8 +1,5 @@
 package tech.bananaz.models;
 
-import static tech.bananaz.encryptors.Security.decrypt;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import lombok.Data;
 import tech.bananaz.utils.TwitterUtils;
@@ -10,10 +7,6 @@ import tech.bananaz.utils.TwitterUtils;
 @Data
 @Component
 public class TwitterConfig {
-	
-	@Value("${bot.encryptionKey}")
-	private String key;
-	private final String NO_KEY_SET = "This application has no internal encryption key set";
 
 	private String address;
 	private String apiKey;
@@ -22,16 +15,12 @@ public class TwitterConfig {
 	private String accessTokenSecret;
 	
 	public TwitterUtils configProperties(Config config) throws Exception {
-		try {
-			// Variables of the twitter root node
-			this.address  		   = config.getContractAddress();
-			this.apiKey 		   = decrypt(key, config.getTwitterApiKey());
-			this.apiSecretKey 	   = decrypt(key, config.getTwitterApiKeySecret());
-			this.accessToken 	   = decrypt(key, config.getTwitterAccessToken());
-			this.accessTokenSecret = decrypt(key, config.getTwitterAccessTokenSecret());
-		} catch (Exception e) {
-			throw new Exception(NO_KEY_SET, e.getCause());
-		}
+		// Variables of the twitter root node
+		this.address  		   = config.getContractAddress();
+		this.apiKey 		   = config.getTwitterApiKey();
+		this.apiSecretKey 	   = config.getTwitterApiKeySecret();
+		this.accessToken 	   = config.getTwitterAccessToken();
+		this.accessTokenSecret = config.getTwitterAccessTokenSecret();
 		
 		return new TwitterUtils(this);
 	}
